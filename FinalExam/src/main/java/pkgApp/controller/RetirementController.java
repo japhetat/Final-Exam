fd862d3e661b8efb33f5a02ae778f93abd91f524
Woodlandadmin <Woodlandadmin@10.0.0.198>
2017-12-10 23:57:35 -0500

    Initial

--- FinalExam/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -6,7 +6,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import pkgApp.RetirementApp;
 
 public class RetirementController implements Initializable {
@@ -17,7 +19,21 @@ public class RetirementController implements Initializable {
 	@FXML
 	private TextField txtYearsToWork;
 	
-
+	@FXML
+	private TextField txtSavingAnnualReturn;
+	
+	@FXML
+	private TextField txtYearsRetired;
+	
+	@FXML
+	private TextField txtReturningAnnualReturn;
+	
+	@FXML
+	private TextField txtRequiredIncome;
+	
+	@FXML
+	private TextField txtMonthlySSI;
+	
 	public RetirementApp getMainApp() {
 		return mainApp;
 	}
@@ -34,14 +50,98 @@ public class RetirementController implements Initializable {
 	public void btnClear(ActionEvent event) {
 		System.out.println("Clear pressed");
 		
+		txtYearsToWork.setText("");
+		txtSavingAnnualReturn.setText("");
+		txtYearsRetired.setText("");
+		txtReturningAnnualReturn.setText("");
+		txtRequiredIncome.setText("");
+		txtMonthlySSI.setText("");
 		//	TODO: Clear all the text inputs
 	}
 	
 	@FXML
 	public void btnCalculate(ActionEvent event) {
 		
+		if (isInputValid()) {
+			
+		}
 		//	TODO: Call AmountToSave and TotalAmountSaved and populate 
 		
+	}
+	
+	private boolean isInputValid() {
+		
+		String errorMessage = "";
+		if (txtYearsToWork.getText() == null || 
+			txtYearsToWork.getText().length() == 0 ||
+			!isValidNumber(txtYearsToWork.getText(), 0, 65)) {
+			errorMessage += "Years of work is not valid!\n";
+		}
+		if (txtSavingAnnualReturn.getText() == null || 
+			txtSavingAnnualReturn.getText().length() == 0 ||
+			!isValidNumber(txtSavingAnnualReturn.getText(), 0, 20)) {
+			errorMessage += "Saving Annual Return is not valid!\n";
+		}
+		if (txtYearsRetired.getText() == null || 
+			txtYearsRetired.getText().length() == 0 ||
+			!isValidNumber(txtYearsRetired.getText(), 0, 65)) {
+			errorMessage += "Years Retired is not valid!\n";
+		}
+		if (txtReturningAnnualReturn.getText() == null || 
+			txtReturningAnnualReturn.getText().length() == 0 ||
+			!isValidNumber(txtReturningAnnualReturn.getText(), 0, 3)) {
+			errorMessage += "Returning Annual Return is not valid!\n";
+		}
+		if (txtRequiredIncome.getText() == null || 
+			txtRequiredIncome.getText().length() == 0 ||
+			!isValidNumber(txtRequiredIncome.getText(), 0, 100)) {
+			errorMessage += "Required Income is not valid!\n";
+		}
+		if (txtMonthlySSI.getText() == null || 
+			txtMonthlySSI.getText().length() == 0 ||
+			!isValidNumber(txtMonthlySSI.getText(), 0, 100)) {
+			errorMessage += "Monthly SSI is not valid!\n";
+		}
+		
+		// TODO validate all fields
+		
+		if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Alert alert = new Alert(AlertType.ERROR);
+            // alert.initOwner(dialogStage);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+
+            return false;
+        }
+		
+	}
+	
+	private boolean isValidNumber(String value, int min, int max) {
+		if (isInteger(value)) {
+			int numberValue = Integer.parseInt(value);
+			if (numberValue >= min && numberValue <= max) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    } catch(NullPointerException e) {
+	        return false;
+	    }
+	    return true;
 	}
 	
 }
--- FinalExam/src/main/java/pkgCore/Retirement.java
+++ FinalExam/src/main/java/pkgCore/Retirement.java
@@ -9,8 +9,108 @@ public class Retirement {
 	private double dRequiredIncome;
 	private double dMonthlySSI;
 	
-	//TODO: Build the contructor, getters and setters for the attributes above.
+	/**
+	 * @param iYearsToWork
+	 * @param dAnnualReturnWorking
+	 * @param iYearsRetired
+	 * @param dAnnualReturnRetired
+	 * @param dRequiredIncome
+	 * @param dMonthlySSI
+	 */
+	public Retirement(int iYearsToWork, double dAnnualReturnWorking, int iYearsRetired, double dAnnualReturnRetired,
+			double dRequiredIncome, double dMonthlySSI) {
+		this.iYearsToWork = iYearsToWork;
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+		this.iYearsRetired = iYearsRetired;
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+		this.dRequiredIncome = dRequiredIncome;
+		this.dMonthlySSI = dMonthlySSI;
+	}
 	
+	/**
+	 * @return the iYearsToWork
+	 */
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+
+	/**
+	 * @param iYearsToWork the iYearsToWork to set
+	 */
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+
+	/**
+	 * @return the dAnnualReturnWorking
+	 */
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+	/**
+	 * @param dAnnualReturnWorking the dAnnualReturnWorking to set
+	 */
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+	/**
+	 * @return the iYearsRetired
+	 */
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+	/**
+	 * @param iYearsRetired the iYearsRetired to set
+	 */
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	/**
+	 * @return the dAnnualReturnRetired
+	 */
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+	/**
+	 * @param dAnnualReturnRetired the dAnnualReturnRetired to set
+	 */
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+	/**
+	 * @return the dRequiredIncome
+	 */
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+	/**
+	 * @param dRequiredIncome the dRequiredIncome to set
+	 */
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+	/**
+	 * @return the dMonthlySSI
+	 */
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+	/**
+	 * @param dMonthlySSI the dMonthlySSI to set
+	 */
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
+	}
+
 	public double AmountToSave()
 	{
 		//TODO: Determine the amount to save each month based on TotalAmountSaved, YearsToWork
